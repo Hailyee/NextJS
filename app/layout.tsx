5dc3cf5499388c91fdb478b8d205d65837cd51f7
Hailyee <a11roundworld@naver.com>
2024-03-01 20:03:19 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Navigation from "../components/navigation"
+import { Metadata } from "next";
+import "../styles/global.css";
+
+export const metadata:Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Next Movies",
+  },
+  description: 'Thr best movies on the best framework'
+}
+
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation/>
+        {children}
+        </body>
+    </html>
+  )
+}
